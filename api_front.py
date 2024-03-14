43fa9e480bd9b64f9a3f44f842a6a89155ce2333
rad$$ <105228117+radss000@users.noreply.github.com>
2024-03-15 00:15:54 +0100

    Update api_front.py

--- api_front.py
+++ api_front.py
@@ -63,29 +63,33 @@ def export_user_data_to_json(user_email, username):
         json_file.write(json_data)
     return json_file_path
 
-def run_process(user_email, username, token, styles):
-    from filt_api_seller import send_email
-    # Vous devez modifier la signature de fetch_and_filter_inventory pour accepter user_email comme argument
-    vinyl_record = scraper.fetch_and_filter_inventory(user_email, username, token, styles)
-    unique_path = f"{username}_{token[:5]}"
-    json_file_path = f"{unique_path}_data.json"
-    output_file_path = f"{unique_path}_analysis.csv"
+from concurrent.futures import ThreadPoolExecutor
 
-    # Sauvegarde des données scrapées en JSON
-    scraper.save_to_firebase(user_email, username, [vinyl_record])
-    json_file_path = export_user_data_to_json(user_email, username)
-    # Application de l'analyse ML et sauvegarde des résultats dans un fichier Excel
-    ml.automate_ml_process(json_file_path, output_file_path)
-    send_email(user_email, output_file_path)
+# Votre fonction run_process doit retourner output_file_path
 
-    # Mettre à jour la variable globale avec le chemin d'accès au fichier généré
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+    username = request.form['username']
+    token = request.form['token']
+    styles = request.form['styles'].split(',')
+    user_email = request.form['user_email']
+
+    # Lancer le processus dans un thread pour ne pas bloquer l'application
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        future = executor.submit(run_process, user_email, username, token, styles)
+        output_file_path = future.result()  # Attend que le thread soit terminé et récupère le résultat
+
+    # Stocker le nom de fichier généré dans une variable globale
     global process_complete_path
     process_complete_path = output_file_path
 
     # Marquer le processus comme terminé
     global process_complete
     process_complete = True
-    return output_file_path
+
+    # Retourner le nom de fichier généré dans la réponse JSON
+    return jsonify({"message": "Processus terminé. Les résultats sont prêts à être téléchargés.", "filename": output_file_path})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
